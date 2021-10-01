9ed40e302c1df4c5ccfc954de5b41a8dcdc93a64
Yer Thao <yyerrthao@gmail.com>
2021-10-01 15:30:31 -0500

    Created 4 operator calculator program

---
+++ better_calculator.rb
@@ -0,0 +1,20 @@
+# Building a better calculator
+
+puts "Enter first number: "
+num1 = gets.chomp().to_f #converted to floating point numbers
+puts "Enter first operator: "
+op = gets.chomp()
+puts "Enter second number: "
+num2 = gets.chomp().to_f #converted to floating point numbers
+
+if op == "+"
+  puts (num1 + num2)
+elsif op == "-"
+  puts (num1 - num2)
+elsif op == "/"
+  puts (num1 / num2)
+elsif op == "*"
+  puts (num1 * num2)
+else
+  puts 'Invalid Operator. Try again.'
+end
