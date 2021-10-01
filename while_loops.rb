38592af26aaa3d1a9fcdc3006da1895b3f79d9dc
Yer Thao <yyerrthao@gmail.com>
2021-10-01 16:02:28 -0500

    While loop notes

---
+++ while_loops.rb
@@ -0,0 +1,19 @@
+# ------------------------ WHILE LOOPS
+# specify a condition
+# while the condition is true, keep looping
+
+
+
+# while the index is less than or equal to five,
+# keep incrementing inside the while loop
+index = 1
+while index <= 5
+  puts index
+  index += 1
+end
+# basically we're saying
+# while index is less than 5
+# increment up from index 1 until index is equal to 5
+
+
+# AVOID INFINITE LOOPS
