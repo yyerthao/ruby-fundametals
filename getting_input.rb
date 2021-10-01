5dcfe519ddf1a010f13655ee26a441be5a242267
Yer Thao <yyerrthao@gmail.com>
2021-10-01 11:38:24 -0500

    Getting inputs notes done

---
+++ getting_input.rb
@@ -0,0 +1,8 @@
+
+# install platforio-ide-terminal within Preferences first
+
+puts "Enter your name: "
+name = gets.chomp() # .chomp() will help us stay on the same line
+puts "Enter your Age: "
+age = gets.chomp()
+puts ("Hello " + name + ", you are " + age)
