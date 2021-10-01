3343162b8d3535515ca99eaadca536c46fd29ea0
Yer Thao <yyerrthao@gmail.com>
2021-10-01 14:52:00 -0500

    If statements notes

---
+++ if_statements.rb
@@ -0,0 +1,21 @@
+# IF STATEMENTS with Dave Chapelle
+
+isfunny= false
+istall = false
+
+#conditional if isFunny is true or not
+ if isfunny and istall
+   puts 'Dave you a tall funny man'
+ elsif isfunny and !istall
+   puts 'dave you a short funny man'
+ elsif !isfunny and istall
+  puts 'You not funny but you tall so alright'
+ else
+   puts 'Dave aint that funny and aint tall either so what\'s up with that'
+ end
+
+
+
+# ------------------------- NOTES -------------------------
+# and ---> used when two statements are checked altogether
+# or --> executes if one of the statements is true
