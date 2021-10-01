5d5ad500e4707d34b89765c275fdc8205377624c
Yer Thao <yyerrthao@gmail.com>
2021-10-01 10:12:46 -0500

    Initial ruby file

---
+++ giraffe.rb
@@ -0,0 +1,12 @@
+# Control R will run this file
+
+# puts "Yer" #this prints out on a new line
+# print "Watermelon" #this puts things on the same line
+
+# Ruby executes through each instruction
+# in the order that we write them
+puts "    /|"
+puts "   / |"
+puts "  /  |"
+puts " /   |"
+puts "/____|"
