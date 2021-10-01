befdf67307dcc4850fc4dfabc1b84aa80b183f49
Yer Thao <yyerrthao@gmail.com>
2021-10-01 11:51:15 -0500

    Madlib program built

---
+++ madlibs.rb
@@ -0,0 +1,14 @@
+
+puts "Enter a color: "
+color = gets.chomp()
+puts "Enter a plural noun"
+plural_noun = gets.chomp()
+puts "Enter a celebrity"
+celebrity = gets.chomp()
+puts "___________________________"
+puts "Here is your madlip result: "
+puts "___________________________"
+puts ("Roses are " + color)
+puts (plural_noun + " are blue")
+puts ("I love " + celebrity)
+puts "___________________________"
